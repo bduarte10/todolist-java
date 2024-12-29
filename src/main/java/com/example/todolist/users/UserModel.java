562e60ba7aecd13ee9acb0e2737b71de8250eb96
Bruno Duarte <duartebruno2208@gmail.com>
2024-12-29 20:17:43 -0300

    first controller

---
+++ src/main/java/com/example/todolist/users/UserModel.java
@@ -0,0 +1,14 @@
+package com.example.todolist.users;
+
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+   private String username;
+   private String name;
+   private String password;
+   
+
+}
